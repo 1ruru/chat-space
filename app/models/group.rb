4e3c37ffcae298b31e600d8fe2100c61eb74b786
Seiji Sakai <iruru147@icloud.com>
2019-03-20 16:52:20 +0900

    [ADD] Model, Controller, Rooting

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,6 @@
 class Group < ApplicationRecord
   validates :name, presence: true
+  has_many :users_groups
   has_many :users, through: :users_groups
   has_many :messages
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :users_groups
   has_many :groups, through: :users_groups
   has_many :messages
 end
