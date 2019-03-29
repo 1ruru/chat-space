cd164299d2935e2187ab481b6058d45468228d00
Seiji Sakai <iruru147@icloud.com>
2019-03-29 17:45:18 +0900

    update groups_controller.rb
    respondを追加

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,6 +4,11 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
     @group.users << current_user
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(name: current_user.name)
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
   def create
     @group = Group.new(group_params)
